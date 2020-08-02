de921cd06a333c87f7f992eb89e445d37f86eb78
Pål Hamel <pal.hamel@gmail.com>
2020-08-02 20:40:41 +0200

    Add axios dep and setup env var for key

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import axios from "axios";
 import { SearchBar } from "./SearchBar";
 
 export class App extends React.Component {
   onSearchSubmit(term) {
-    console.log('from App comp:', term)
+    // console.log('from App comp:', term)
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`,
+      },
+    });
   }
-
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
